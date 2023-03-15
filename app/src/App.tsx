300e43bfbdb8baed8307acb166ce7c0c06894678
David <DavidMelnychuk@users.noreply.github.com>
2023-03-14 23:56:43 -0700

    feat: redo challenge with tailwind

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,14 +3,16 @@ import QRCodeImage from "./assets/image-qr-code.png";
 
 function App() {
   return (
-    <div className="card">
+    <div className="p-4 rounded-2xl w-80 bg-white shadow-lg">
       <img
         src={QRCodeImage}
-        className="qr-code"
+        className="rounded-2xl w-full"
         alt="QR Code to Frontend Mentor"
       ></img>
-      <h2>Improve your front-end skills by building projects</h2>
-      <p>
+      <h2 className="text-[#1f3251] text-2xl m-2 mt-4 font-bold">
+        Improve your front-end skills by building projects
+      </h2>
+      <p className="text-[#7b879d] text-base font-normal leading-5 pb-4">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
